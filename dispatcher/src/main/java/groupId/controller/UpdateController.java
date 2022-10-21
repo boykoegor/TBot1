fd6ba2c84ebfbb573522e40976361215e9a4b118
Egor <boyko.e.r@gmail.com>
2022-10-21 18:37:00 +0300

    add response sending message about unsupported file for this update

--- dispatcher/src/main/java/groupId/controller/UpdateController.java
+++ dispatcher/src/main/java/groupId/controller/UpdateController.java
@@ -1,13 +1,20 @@
 package groupId.controller;
 
+import groupId.utils.MessageUtils;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 @Log4j
 public class UpdateController {
     private TelegramBot telegramBot;
+    private MessageUtils messageUtils;
+
+    public UpdateController(MessageUtils messageUtils) {
+        this.messageUtils = messageUtils;
+    }
 
     public void registerBot(TelegramBot telegramBot){
         this.telegramBot = telegramBot;
@@ -40,6 +47,13 @@ public class UpdateController {
     }
 
     private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Неподдерживаемый формат файла");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
     }
 
     private void processPhotoMessage(Update update) {
